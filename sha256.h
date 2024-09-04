84238499b58633bb432ceae1977b2a6d4a9df90e
Martín Vigil <gagliotti@gmail.com>
2024-09-04 14:08:30 -0300

    Add files via upload

--- sha256.h
+++ sha256.h
@@ -9,11 +9,20 @@
 #ifndef sha256_hpp
 #define sha256_hpp
 
+#if OPENSSL_VERSION_NUMBER >= 0x3000000000L
+
+#include <openssl/evp.h>
+
+#else
+
 #include <stdio.h>
 #include <openssl/sha.h>
 #include <openssl/bio.h>
-#include <string>
 
+
+#endif
+
+#include <string>
 std::string printSha256(const char *path);
 
 
