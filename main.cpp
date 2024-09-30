cdd7d9ec139ec6e6607121488bb820f5a90eb991
Martín Vigil <gagliotti@gmail.com>
2024-09-30 16:37:33 +0000

    removi define

--- main.cpp
+++ main.cpp
@@ -4,20 +4,20 @@
 #include <chrono>
 #include <thread>
 
-#define CHAVE 10
+const int CHAVE = 10;
 
 void produtor()
 {
-  int mem_id, *ptr_mem;
+  int mem_id = shmget(CHAVE, sizeof(int) * 256, 0777 | IPC_CREAT);
 
-  mem_id = shmget(CHAVE, sizeof(int) * 256, 0777 | IPC_CREAT);
   if (mem_id < 0)
   {
     std::cerr << "Erro ao criar area de memoria compartilhada..." << std::endl;
     exit(0);
   }
 
-  ptr_mem = (int *)shmat(mem_id, nullptr, 0);
+  int *ptr_mem = (int *)shmat(mem_id, nullptr, 0);
+
   if (ptr_mem == nullptr)
   {
     std::cerr << "Erro de mapeamento de memoria..." << std::endl;
