30f34537dbd6822b882230b51fb4d32fafd54fa4
Martín Vigil <gagliotti@gmail.com>
2025-09-01 13:21:57 +0000

    adicionei corpo das funções

--- fs.cpp
+++ fs.cpp
@@ -3,3 +3,52 @@
  */
 
 #include "fs.h"
+
+void initFs(std::string fsFileName, int blockSize, int numBlocks, int numInodes)
+{
+    //implemente aqui
+};
+
+/**
+ * @brief Adiciona um novo arquivo dentro do sistema de arquivos que simula EXT3. O sistema já deve ter sido inicializado.
+ * @param fsFileName arquivo que contém um sistema sistema de arquivos que simula EXT3.
+ * @param filePath caminho completo novo arquivo dentro sistema de arquivos que simula EXT3.
+ * @param fileContent conteúdo do novo arquivo
+ */
+void addFile(std::string fsFileName, std::string filePath, std::string fileContent)
+{
+    //implemente aqui
+};
+
+/**
+ * @brief Adiciona um novo diretório dentro do sistema de arquivos que simula EXT3. O sistema já deve ter sido inicializado.
+ * @param fsFileName arquivo que contém um sistema sistema de arquivos que simula EXT3.
+ * @param dirPath caminho completo novo diretório dentro sistema de arquivos que simula EXT3.
+ */
+void addDir(std::string fsFileName, std::string dirPath)
+{
+    //implemente aqui
+};
+
+/**
+ * @brief Remove um arquivo ou diretório (recursivamente) de um sistema de arquivos que simula EXT3. O sistema já deve ter sido inicializado.
+ * @param fsFileName arquivo que contém um sistema sistema de arquivos que simula EXT3.
+ * @param path caminho completo do arquivo ou diretório a ser removido.
+ */
+void remove(std::string fsFileName, std::string path)
+{
+    //implemente aqui
+};
+
+/**
+ * @brief Move um arquivo ou diretório em um sistema de arquivos que simula EXT3. O sistema já deve ter sido inicializado.
+ * @param fsFileName arquivo que contém um sistema sistema de arquivos que simula EXT3.
+ * @param oldPath caminho completo do arquivo ou diretório a ser movido.
+ * @param newPath novo caminho completo do arquivo ou diretório.
+ */
+void move(std::string fsFileName, std::string oldPath, std::string newPath)
+{
+    //implemente aqui
+};
+
+
